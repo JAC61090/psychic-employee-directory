b53e90e9e3ff2df5137810696c0c6251fd21f85c
Jason carrazco <carrazcojason10@gmail.com>
2021-06-05 16:31:03 -0500

    clearing files

--- employeedirectory/src/utils/API.js
+++ employeedirectory/src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getRandomser: function () {
-        return axios.get("https://randomuser.me/");
+        return axios.get("https://randomuser.me/api/?results=25&nat=us");
     }
 }
