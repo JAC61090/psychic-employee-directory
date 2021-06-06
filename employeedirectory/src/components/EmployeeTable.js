547e037b26066f4e36cc5851ab20c9ae9e077f74
Jason carrazco <carrazcojason10@gmail.com>
2021-06-05 20:22:45 -0500

    factored main employee table functions

--- employeedirectory/src/components/EmployeeTable.js
+++ employeedirectory/src/components/EmployeeTable.js
@@ -1,38 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import API from "../utils/API.js";
 
 function EmployeeTable() {
-    return (
-        <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+  const [randomUser, setRandomUser] = useState([]);
 
-    );
+  useEffect(() => {
+    fillEmployeeTable();
+  }, []);
+
+  const fillEmployeeTable = async () => {
+    const res = await API.getRandomUsers();
+    await setRandomUser(res.data.results);
+    console.log(...randomUser);
+  };
+
+  const sortAlphabetically = (e) => {
+    function compare(a, b) {
+      if (a.name.last < b.name.last) {
+        return -1;
+      }
+      if (a.name.last > b.name.last) {
+        return 1;
+      }
+      return 0;
+    }
+    var sortedUser = randomUser.sort(compare);
+    setRandomUser([...sortedUser]);
+  };
+  
+  const filterAge = (e) => {
+    var peopleSeventyTwo = [];
+    for (let index = 0; index < randomUser.length; index++) {
+      const element = randomUser[index];
+      if (randomUser[index].dob.age > 72) {
+        peopleSeventyTwo.push(randomUser[index]);
+      }
+    }
+    // var sortedUser = randomUser.sort(compare);
+    setRandomUser([...peopleSeventyTwo]);
+  };
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Photo</th>
+          <th scope="col">First</th>
+          <th scope="col"> <button onClick={sortAlphabetically}>Last</button></th>
+          <th scope="col">
+            <button onClick={filterAge}>Age</button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* map around this row */}
+        {randomUser.map((result) => (
+          <tr>
+            <th scope="row">
+              <img src={result.picture.thumbnail} alt=""></img>
+            </th>
+            <td>{result.name.first}</td>
+            <td>{result.name.last}</td>
+            <td>{result.dob.age}</td>
+          </tr>
+        ))}
+        {/* end map */}
+      </tbody>
+    </table>
+  );
 }
 
 export default EmployeeTable;
